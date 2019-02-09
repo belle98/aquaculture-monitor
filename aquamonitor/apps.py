a9d462cb00298381f0d9e3fddb8ed61d9f587718
Almee <almee6198@gmail.com>
2019-02-09 10:25:17 +0530

    My Aquamonitor app, first commit

---
+++ aquamonitor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AquamonitorConfig(AppConfig):
+    name = 'aquamonitor'
---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Python Project\project\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
